8d3702ecc699168cfb1fb475c3f6fdddc2488d93
Davi Henrique <davihenriquefavaro@gmail.com>
2021-02-22 17:08:31 +0000

    code day one

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './style/global.css';
+import { ExperienceBar } from './components/Experiencebar';
+
+function App() {
+  return (
+      <div className='container'>
+     <ExperienceBar />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/components/Experiencebar.tsx
@@ -0,0 +1,13 @@
+export function ExperienceBar() {
+    return (
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{width: '58%'}} />
+
+                <span className='currence-experience' style={{ left: '58%'}} >300px</span>
+            </div>
+            <span>600 xp</span>
+        </header>
+    );
+} 
