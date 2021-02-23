0ac5f3075f8d54563e7bd177e331096b4b489512
Davi Henrique <davihenriquefavaro@gmail.com>
2021-02-23 20:14:47 +0000

    migrate nextjs

--- src/components/Experiencebar.tsx
+++ src/components/Experiencebar.tsx
@@ -1,11 +1,14 @@
+import styles from "../styles/components/ExperienceBar.module.css";
+
+
 export function ExperienceBar() {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
                 <div style={{width: '58%'}} />
 
-                <span className='currence-experience' style={{ left: '58%'}} >300px</span>
+                <span className={styles.currenceExperience} style={{ left: '58%'}} >300px</span>
             </div>
             <span>600 xp</span>
         </header>
---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+export default function Profile() {
+    return (
+        <div>
+            <img src="https://github.com/davifa1.png" alt="Davi Henrique"/>
+            <div>
+                <strong>
+                    Davi Henrique
+                </strong>
+                <p>Level: 1</p>
+            </div>
+        </div>
+    );
+}
